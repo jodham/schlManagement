5f107761da58407e3e36a1fe6bbb45be60481fbe
wanjalajodham@gmail.com <wanjalajodham@gmail.com>
2020-12-16 23:22:20 +0300

    new

---
+++ newAccount.py
@@ -0,0 +1,49 @@
+from tkinter import *
+
+root = Tk()
+root.geometry("300x400")
+root.title("new account")
+label = Label(root, text="Create Account",font="times 12 bold")
+label.place(x=70,y=15)
+
+label1 = Label(root, text="First Name",font="times 12 bold")
+label1.place(x=50,y=50)
+
+e1= Entry(root)
+e1.place(x=50, y=70)
+
+label2 = Label(root, text="second Name",font="times 12 bold")
+label2.place(x=50,y=90)
+
+e2= Entry(root)
+e2.place(x=50, y=115)
+
+label3 = Label(root, text="Username",font="times 12 bold")
+label3.place(x=50,y=140)
+
+e3= Entry(root)
+e3.place(x=50, y=170)
+
+label4 = Label(root, text="Password",font="times 12 bold")
+label4.place(x=50,y=200)
+
+passEntry = Entry(root, show="*")
+passEntry.place(x=50, y=230)
+
+label5 = Label(root, text="Re-Enter Password",font="times 12 bold")
+label5.place(x=50,y=260)
+
+passEntry1 = Entry(root, show="*")
+passEntry1.place(x=50, y=290)
+
+
+label6 = Label(root, text="Subject",font="times 12 bold")
+label6.place(x=50,y=320)
+
+e4 = Entry(root)
+e4.place(x=50, y=350)
+
+btn = Button(root, text="Sign In", font="times 15 bold")
+btn.place(x=200,y=355)
+
+root.mainloop()
