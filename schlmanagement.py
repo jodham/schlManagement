a3d85485fc83ebdf32949fe5777d866d8a928d41
wanjalajodham@gmail.com <wanjalajodham@gmail.com>
2020-12-19 14:07:31 +0300

    linked account login_forms

--- schlmanagement.py
+++ schlmanagement.py
@@ -5,6 +5,7 @@ window = Tk()
 window.geometry("700x500")
 window.title("welcome screen")
 
+#create account form___________________________start
 def create_account():
     root = Toplevel(window)
     root.title("create account")
@@ -51,11 +52,96 @@ def create_account():
 
     btn = Button(root, text="Sign In", font="times 15 bold")
     btn.place(x=200, y=355)
+#create account form--------------------------------end
 
 
-def student_form():
-    print("student account")
+#student login form---------------------start
 
+
+def student_login():
+    stud_login = Toplevel(window)
+    stud_login.geometry("500x500")
+    stud_login.title("login screen")
+    lb1 = Label(stud_login, text="Enter Login Details", font="times 15 bold")
+    lb1.place(x=160, y=10)
+
+    lb2 = Label(stud_login, text="Username", font=" times 15 bold")
+    lb2.place(x=160, y=80)
+
+    e1 = Entry(stud_login)
+    e1.place(x=160, y=110)
+
+    lb3 = Label(stud_login, text="Password", font=" times 15 bold")
+    lb3.place(x=160, y=140)
+
+    passEntry = Entry(stud_login, show="*")
+    passEntry.place(x=160, y=170)
+
+    btn = Button(stud_login, text="Login", font="times 15 bold")
+    btn.place(x=160, y=200)
+
+    btn1 = Button(stud_login, text="forgot password", font="times 10")
+    btn1.place(x=160, y=255)
+#student login form ---------------------------------end
+
+
+#teacher login form ----------------------------------start
+def teach_login():
+    teacher_login = Toplevel()
+    teacher_login.geometry("500x500")
+    teacher_login.title("login screen")
+    lb1 = Label(teacher_login, text="Enter Login Details", font="times 15 bold")
+    lb1.place(x=160, y=10)
+
+    lb2 = Label(teacher_login, text="Username", font=" times 15 bold")
+    lb2.place(x=160, y=80)
+
+    e1 = Entry(teacher_login)
+    e1.place(x=160, y=110)
+
+    lb3 = Label(teacher_login, text="Password", font=" times 15 bold")
+    lb3.place(x=160, y=140)
+
+    passEntry = Entry(teacher_login, show="*")
+    passEntry.place(x=160, y=170)
+
+    btn = Button(teacher_login, text="Login", font="times 15 bold")
+    btn.place(x=160, y=200)
+
+    btn1 = Button(teacher_login, text="forgot password", font="times 10")
+    btn1.place(x=160, y=255)
+
+
+#teacher login form ------------------------------------end
+
+#worker login form --------------------------------------start
+def worker_account():
+    worker_login = Toplevel()
+    worker_login.geometry("500x500")
+    worker_login.title("login screen")
+    lb1 = Label(worker_login, text="Enter Login Details", font="times 15 bold")
+    lb1.place(x=160, y=10)
+
+    lb2 = Label(worker_login, text="Username", font=" times 15 bold")
+    lb2.place(x=160, y=80)
+
+    e1 = Entry(worker_login)
+    e1.place(x=160, y=110)
+
+    lb3 = Label(worker_login, text="Password", font=" times 15 bold")
+    lb3.place(x=160, y=140)
+
+    passEntry = Entry(worker_login, show="*")
+    passEntry.place(x=160, y=170)
+
+    btn = Button(worker_login, text="Login", font="times 15 bold")
+    btn.place(x=160, y=200)
+
+    btn1 = Button(worker_login, text="forgot password", font="times 10")
+    btn1.place(x=160, y=255)
+
+
+#worker login form ------------------------------------------end
 lb1 = Label(window, text="Welcome to Excel High School", font="times 20 bold")
 lb1.place(x=180, y=10)
 
@@ -68,13 +154,13 @@ lb3.place(x=280, y=80)
 lb4 = Label(window, text="Login As", font="times 15 bold")
 lb4.place(x=280, y=140)
 
-btn1 = Button(window, text="Student", font="times 15 bold")
+btn1 = Button(window, text="Student",command=student_login, font="times 15 bold")
 btn1.place(x=200, y=200)
 
-btn2 = Button(window, text="Teacher", font="times 15 bold")
+btn2 = Button(window, text="Teacher",command = teach_login, font="times 15 bold")
 btn2.place(x=320, y=200)
 
-btn3 = Button(window, text="Worker", font="times 15 bold")
+btn3 = Button(window, text="Worker",command = worker_account, font="times 15 bold")
 btn3.place(x=445, y=200)
 
 btn4 = Button(window, text="Create account",command=create_account, font="times 12 bold")
